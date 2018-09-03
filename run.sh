8013b76bb625b484564e05577fea74fd7a9419c1
javid <javiddadashkarimi@gmail.com>
2018-09-03 11:43:27 -0400

    adding new post

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cp $1 ./_posts
 
 git branch svgs # if this isn't already there
 #python -m readme2tex --output READMETMP.md $input --username dadashkarimi --branch master --project dadashkarimi.github.io --usepackage amsmath --usepackage amssymb --usepackage caption
-python -m readme2tex --output READMETMP.md $input --username dadashkarimi --branch svgs --project dadashkarimi.github.io --usepackage amsmath
+python -m readme2tex --output READMETMP.md $input --username dadashkarimi --branch master --project dadashkarimi.github.io --usepackage amsmath
 mv READMETMP.md $input
 git add $input
 git add svgs
